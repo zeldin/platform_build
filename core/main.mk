ef1fbd3348533cc369339e45af87a61630806196
Marcus Comstedt <marcus@mc.pp.se>
2015-09-09 21:16:20 +0200

    Identify IcedTea as OpenJDK

--- core/main.mk
+++ core/main.mk
@@ -196,7 +196,7 @@ endif
 ifeq ($(requires_openjdk), true)
 # The user asked for java7 openjdk, so check that the host
 # java version is really openjdk
-ifeq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
+ifeq ($(shell echo '$(java_version_str)' | grep -i openjdk\\\|icedtea),)
 $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
@@ -204,7 +204,7 @@ $(info ************************************************************)
 $(error stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
-ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
+ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk\\\|icedtea),)
 $(info ************************************************************)
 $(info You are attempting to build with an unsupported JDK.)
 $(info $(space))
